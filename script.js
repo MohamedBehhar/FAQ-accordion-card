496e3b0577f4c3c683647f43503975b875390cd8
Mohamed Behhar <mbehhar@e2r4p10.1337.ma>
2021-12-29 11:44:36 +0100

    Modifying style and js to make it more like the orginal design

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const questions = document.querySelectorAll(".question");
-const answer	= document.querySelectorAll('.answer');
-const arrow		= document.querySelectorAll('.arrow');
-
-
-// questions.forEach((question) => {
-// 	const btns
-// })
 
 questions.forEach(function(question){
 	const btn		= question.querySelector('.qst-btn');
@@ -14,18 +7,8 @@ questions.forEach(function(question){
 	const openAnswer = question.querySelector('.answer');
 
 	btn.addEventListener('click', function(){
-		answer.forEach((item) =>{
-			console.log(question.lastElementChild);
-			// // console.log(item);
-			// if (question.lastElementChild !== item)
-			// {
-			// 	item.classList.remove('show-answer')
-			// 	arrow.classList.remove('rotate-arrow');
-			// 	header.classList.remove('header-bold');
-			// }
-			question.lastElementChild.classList.toggle('show-answer');
+			openAnswer.classList.toggle('show-answer');
 			arrow.classList.toggle('rotate-arrow');
 			header.classList.toggle('header-bold');
-		})
 	})
 })
